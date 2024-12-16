ea903ba19c7cd9b8e4234518cb2914d651585d78
Brandon Eastwell <be21160@essex.ac.uk>
2024-12-16 20:52:18 +0000

    created managers for handling of forms and page states

---
+++ src/scripts/formManager.js
@@ -0,0 +1,41 @@
+class FormManager {
+    constructor(controller) {
+        this.controller = controller;
+    }
+
+    displayTodoForm() {
+        const form = formList('todo');
+        if (form) {
+            form.addEventListener("submit", this.handleTodoSubmit.bind(this));
+            document.body.appendChild(form);
+        }
+    }
+
+    displayProjectForm() {
+        const form = formList('project');
+        if (form) {
+            form.addEventListener("submit", this.handleProjectSubmit.bind(this));
+            document.body.appendChild(form);
+        }
+    }
+
+    handleTodoSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        this.controller.createTodoList(
+            data.get("name"),
+            data.get("desc"),
+            data.get("dueDate"),
+            data.get("priority")
+        );
+    }
+
+    handleProjectSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        this.controller.createProject(
+            data.get("name"),
+            data.get("desc")
+        );
+    }
+}
